5a6700c68e246647a302a07ee1a6fa6721f2686f
Anton2781618 <69422633+Anton2781618@users.noreply.github.com>
2025-09-16 12:34:59 +0300

    Initial commit

---
+++ Assets/Scripts/Installer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class Installer : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        
+    }
+        
+}
